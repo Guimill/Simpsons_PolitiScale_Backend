71836afb90bd4b90908c96b160f3d44b007f0592
Guimill <86968380+Guimill@users.noreply.github.com>
2023-03-13 21:30:58 +0100

    test deploy

--- config/config.js
+++ config/config.js
@@ -1,11 +1,12 @@
 // connection à la db
 const Sequelize = require("sequelize");
 module.exports = new Sequelize(
-    'Simpsons',
-    'root',
-    'Universum555-',
+    'heroku_176c9b11cb265e6',
+    'b9eb7622aba688',
+    '710c7d94',
      {
-       host: 'localhost',
+       host: 'eu-cdbr-west-03.cleardb.net',
        dialect: 'mysql'
      }
    );
+
